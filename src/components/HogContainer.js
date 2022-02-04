48660a6d47870a355e43b52b40715c1425dfdced
Dalton <slattery.da@gmail.com>
2022-02-04 09:24:12 -0600

    idk what happened

--- src/components/HogContainer.js
+++ src/components/HogContainer.js
@@ -1,12 +1,16 @@
 import React,{useState} from 'react';
-import PigCard from './PigCard';
+import PigInfo from './PigInfo';
+import PigPhoto from './PigPhoto';
 
 function HogContainer({hogs}) {
-
-  return (
-    <div>
-        <PigCard hogs={hogs} />
-    </div>);
+    const [imageOn, setImage] = useState(true)
+    function handleClick(){
+        setImage(!imageOn)
+    }
+    return (
+        <ul>
+        {imageOn ? <PigPhoto hogs={hogs} handleClick={handleClick} imageOn={imageOn}/> : <PigInfo handleClick={handleClick} hogs={hogs} imageOn={imageOn}/>}
+        </ul>)
 }
 
 export default HogContainer;
